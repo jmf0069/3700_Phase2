5403a037afacadace1d135d0fc43781960bbfb51
Theysis <44480056+Theysis@users.noreply.github.com>
2021-04-17 18:08:54 -0500

    Add files via upload

---
+++ Group.py
@@ -0,0 +1,42 @@
+# # TEAM 5 
+# JOSHUA WYATT, BENJAMIN RHEE, NATHAN BIAGIONI, ALLEN CARTER, AND JAKE FARLEY
+#
+# Group class serves as foundation for the grouping of accounts
+class Group :
+
+# Variables
+gName
+preferredRegion
+gSize : int
+preferredGames : Array
+owner : admin
+members : Array
+
+# Constructor ?
+def Group() : 
+
+# Returns array of preferred region listed by the group
+def getPreferedRegion() :
+    return 
+
+# 
+def setPreferredRegion(region) : 
+
+
+# Returns name associated with the group
+def getGroupName() :
+    return gName
+
+# Used in group creation, sets the name of the group if the name is not already taken.
+def setGroupName(groupName) :
+
+# Returns current group size
+def getGroupSize() :
+    return gSize
+
+# Is capable of being changed at any time by the admin, sets the maximum group size
+def setGroupSize() : 
+
+# 
+def sendInvite(username) :
+    
---
+++ Player.py
@@ -0,0 +1,13 @@
+# TEAM 5 
+# JOSHUA WYATT, BENJAMIN RHEE, NATHAN BIAGIONI, ALLEN CARTER, AND JAKE FARLEY
+#
+# Player class serves as foundation for 
+class Player :
+
+# Variables
+
+#
+def login(userName, password) :
+
+# Allows the user to create an account
+def createAccount(userName, password) :
