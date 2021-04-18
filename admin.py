e0b37529a503522c2a00d918862767c3fa35f8b9
NebRhee <82688937+NebRhee@users.noreply.github.com>
2021-04-18 14:23:09 -0500

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,25 @@
+# TEAM 5 
+# JOSHUA WYATT, BENJAMIN RHEE, NATHAN BIAGIONI, ALLEN CARTER, AND JAKE FARLEY
+#
+# Admin Class is a type of Account
+
+class Admin : 
+
+
+# Constructor
+def __init__(self) : 
+
+def getPreferredRegion() : 
+
+def setPreferredRegion() : 
+
+def getGroupName() : 
+
+def setGroupName(groupName) : 
+    groupName = gName
+
+def getGroupSize() : 
+    return gSize 
+
+def setGroupSize(size) : 
+    gSize = size
