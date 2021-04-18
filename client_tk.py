cc035800025e8a8b9ffc1e69690ac200bb8c0b00
far <jmf0069@auburn.edu>
2021-04-17 20:19:05 -0500

    Working well now. Just need to link it to classes.

--- client_tk.py
+++ client_tk.py
@@ -1,12 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
 
-
+# Application Class: The client for the user. Relies on other classes.
 class Application(Frame):
 
+    # login(self) Funtion: Takes two entries from user (username and password), 
+    # and upon submission the credentials are checked with the backend to 
+    # see if login is valid.
     def login(self):
         self.Frame2 = Frame(root, padx=5, pady=5)
-        self.Frame2.grid(row=0, column=1)
+        self.Frame2.place(relx=0.435, rely=0.255, relwidth=0.36, relheight=0.18)
         self.Frame2.configure(relief=GROOVE)
         self.Frame2.configure(borderwidth="2")
         self.Frame2.configure(relief=GROOVE)
@@ -14,17 +17,23 @@ class Application(Frame):
         self.Frame2.configure(highlightbackground="#ffffff")
         self.Frame2.configure(highlightcolor="black")
         label1 = Label(self.Frame2, text='Username:', padx=5, pady=5)
-        label1.grid(row=0, column=1)
+        label1.grid(row=0, column=2)
         e1 = Entry(self.Frame2)
-        e1.grid(row=0, column=2)
+        e1.grid(row=0, column=3)
         label2 = Label(self.Frame2, text='Password:', padx=5, pady=5)
-        label2.grid(row=1, column=1)
+        label2.grid(row=1, column=2)
         e2 = Entry(self.Frame2)
-        e2.grid(row=1, column=2)
+        e2.grid(row=1, column=3)
+        self.submitlogin = Button(self.Frame2,
+                                  text="Submit") #fix this ahhhhhhhhhhhhhh
+        self.submitlogin.grid(row=3, column=3)
 
+    # register_acc(self) Function: Takes (3) entries from user (username, 
+    # password, and password confirmation), ensures that the passwords
+    # match, and submits the info to the backend so user can login.
     def register_acc(self):
         self.Frame3 = Frame(root, padx=5, pady=5)
-        self.Frame3.grid(row=0, column=1)
+        self.Frame3.place(relx=0.435, rely=0.255, relwidth=0.36, relheight=0.18)
         self.Frame3.configure(relief=GROOVE)
         self.Frame3.configure(borderwidth="2")
         self.Frame3.configure(relief=GROOVE)
@@ -33,8 +42,8 @@ class Application(Frame):
         self.Frame3.configure(highlightcolor="black")
         label1 = Label(self.Frame3, text='Username:', padx=5, pady=5)
         label1.grid(row=0, column=1)
-        user = Entry(self.Frame3)
-        user.grid(row=0, column=2)
+        user1 = Entry(self.Frame3)
+        user1.grid(row=0, column=2)
         label2 = Label(self.Frame3, text='Password:', padx=5, pady=5)
         label2.grid(row=1, column=1)
         pass1 = Entry(self.Frame3)
@@ -44,55 +53,58 @@ class Application(Frame):
         pass2 = Entry(self.Frame3)
         pass2.grid(row=2, column=2)
         self.submitreg = Button(self.Frame3,
-                    text="Submit",
-                    command=partial(verifyPasswordRegister, user, pass1, pass2)) #fix this ahhhhhhhhhhhhhh
+                                text="Submit",
+                                command=lambda: (self.verifyPasswordRegister(user1, pass1, pass2)))
         self.submitreg.grid(row=3, column=2)
 
 
-    def registerAccount(self):
-        username = self.register_acc.e1
-        password = self.register_acc.e2
+    # def registerAccount(self):
+    #     username = self.register_acc.user1
+    #     password = self.register_acc.pass1
     
-    def verifyPasswordRegister(user, pass1, pass2):
-        if (self.pass1 != self.pass2):
-            messagebox.showerror("showerror", "Passwords do not match. Try again.")
+    # verifyPasswordRegister(...) Function: Checks if passwords match.
+    def verifyPasswordRegister(self, user1, pass1, pass2):
+        if (pass1.get() == pass2.get()):
+            messagebox.showinfo("showinfo", "Congratulations! You are registered.")
+            user1.delete(0, 'end')
+            pass1.delete(0, 'end')
+            pass2.delete(0, 'end')
+            self.Frame3.place_forget()
+            self.Frame3.destroy()
+            self.login()
         else:
-            messagebox.showinfo("showinfo", "Congratulations, " + user + "! You are registered.")
-        
-
+            messagebox.showerror("showerror", "Passwords do not match. Try again.")
+            pass1.delete(0, 'end')
+            pass2.delete(0, 'end')
+    
+    # createWidgets(self) Function: Creates the main widgets for the client.
     def createWidgets(self):
         self.Frame1 = Frame(root, padx=5, pady=5)
-        self.Frame1.grid(row=0, column=0)
+        self.Frame1.place(relx=0.12, rely=0.1, relwidth=0.75, relheight=0.5)
         self.Frame1.configure(relief=GROOVE)
         self.Frame1.configure(borderwidth="2")
         self.Frame1.configure(relief=GROOVE)
         self.Frame1.configure(background="#ffffff")
         self.Frame1.configure(highlightbackground="#ffffff")
         self.Frame1.configure(highlightcolor="black")
-        # self.Frame1.configure(width=720)
-
-        # self.Frame1.rowconfigure(0, weight=1, minsize=20)
-        # self.Frame1.columnconfigure(0, weight=1, minsize=4)
-
-        self.login = Button(self.Frame1,
-                        text="Login",
-                        command=self.login)
-        self.login.grid(row=0, column=0)
+        self.loginButton = Button(self.Frame1,
+                                  text="Login",
+                                  command=self.login)
+        self.loginButton.place(relx=0.12, rely=0.3, relwidth=0.2, relheight=0.1)
 
         self.register = Button(self.Frame1,
-                        text="Register",
-                        command=self.register_acc)
-        self.register.grid(row=1, column=0)
+                               text="Register",
+                               command=self.register_acc)
+        self.register.place(relx=0.12, rely=0.41, relwidth=0.2, relheight=0.1)
 
         self.QUIT = Button(self.Frame1,
-                    text="EXIT",
-                    fg="red",
-                    command=self.quit)
-        self.QUIT.grid(row=2, column=0)
+                           text="EXIT",
+                           fg="red",
+                           command=self.quit)
+        self.QUIT.place(relx=0.12, rely=0.52, relwidth=0.2, relheight=0.1)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        # self.pack()
         self.createWidgets()
 
 root = Tk()
