ac889c00adcdb37daf1d37455e130a16bc90bab1
CIRCREPORTER <CIRCREPORTER@auburn.edu>
2021-04-19 10:53:57 -0500

    Almost finished. Group system in place. Need to add deleting/adding

--- client_tk.py
+++ client_tk.py
@@ -4,8 +4,8 @@ from tkinter import messagebox
 # Application Class: The client for the user.
 class Application(Frame):
 
-    # login(self) Funtion: Takes two entries from user (username and password), 
-    # and upon submission the credentials are checked with the backend to 
+    # login(self) Funtion: Takes two entries from user (username and password),
+    # and upon submission the credentials are checked with the backend to
     # see if login is valid.
     def login(self):
         self.LoginFrame = Frame(root, padx=5, pady=5)
@@ -26,7 +26,7 @@ class Application(Frame):
         e2.grid(row=1, column=3)
         self.submitlogin = Button(self.LoginFrame,
                                   text="Submit",
-                                  command=lambda: (self.checkLoginCred(e1, e2))) #, self.setUsername(e1.get())
+                                  command=lambda: (self.checkLoginCred(e1, e2)))
         self.submitlogin.grid(row=3, column=3)
 
     def add_account(self, user, password):
@@ -60,6 +60,7 @@ class Application(Frame):
         if (e1 != ""):
             if (e2 != ""):
                 if (self.is_registered(e1.get(), e2.get())):
+                    self.setUsername(e1.get())
                     self.postLoginScreen()
                 else:
                     messagebox.showerror("showerror", "Invalid username and/or password.")
@@ -84,23 +85,23 @@ class Application(Frame):
         self.PostLoginFrame.configure(background="#ffffff")
         self.PostLoginFrame.configure(highlightbackground="#ffffff")
         self.PostLoginFrame.configure(highlightcolor="black")
-        self.createGroupButton = Button(self.PostLoginFrame, 
-                                    text='Create Group', 
-                                    padx=20, pady=20,
-                                    command=lambda: self.createGroup())
+        self.createGroupButton = Button(self.PostLoginFrame,
+                                        text='Create Group',
+                                        padx=20, pady=20,
+                                        command=lambda: self.createGroup())
         self.createGroupButton.pack(side=TOP, expand=True, fill=BOTH)
-        self.manageGroupButton = Button(self.PostLoginFrame, 
-                                    text='Manage Group', 
-                                    padx=20, pady=20,
-                                    command=lambda: self.manageGroup())
+        self.manageGroupButton = Button(self.PostLoginFrame,
+                                        text='Manage Group',
+                                        padx=20, pady=20,
+                                        command=lambda: self.manageGroup())
         self.manageGroupButton.pack(side=TOP, expand=True, fill=BOTH)
-        self.logoutButton = Button(self.PostLoginFrame, 
-                                    text='LOGOUT', 
-                                    fg='red',
-                                    padx=20, pady=20,
-                                    command=lambda: self.logout())
+        self.logoutButton = Button(self.PostLoginFrame,
+                                        text='LOGOUT',
+                                        fg='red',
+                                        padx=20, pady=20,
+                                        command=lambda: self.logout())
         self.logoutButton.pack(side=TOP, expand=True, fill=BOTH)
-                
+
     def logout(self):
         self.PostLoginFrame.place_forget()
         self.createWidgets()
@@ -112,7 +113,7 @@ class Application(Frame):
                 # This expects each line of a file to be (name pass) seperated by whitespace
                 group_array.append(line.split())
                 yield group_array
-    
+
     def groupExists(self, groupname):
         groups = []
         with open(GROUP_DATA, "r") as data:
@@ -123,16 +124,28 @@ class Application(Frame):
             return False
 
     def addGroup(self, group):
-        with open(GROUP_DATA, "a") as data:
-            data.write(group[0] + " " + group[1] + " " + group[2] + " " + group[3] + " " + group[4] + " " + group[5] +"\n")
+        with open(USER_DATA, "r") as udata:
+            for line in udata.readlines():
+                user, password = line.split()
+                if (user == group[1]):
+                    messagebox.showerror("showerror", "You are already in a group.\nTo make another one, you must leave your current one.")
+        with open(GROUP_DATA, "a") as gdata:
+            gdata.write(group[0] + " " + group[1] + " " + group[2] + " " + group[3] + " " + group[4] + " " + group[5] +"\n")
 
     def checkGroupExists(self, groupname, group):
         if (self.groupExists(groupname)):
             messagebox.showerror("showerror", "Group name is taken. Please try another.")
         else:
-            # messagebox.showinfo("showinfo", "Your group, " + groupname + ", has been created!")
-            self.addGroup(group)
-            
+            messagebox.showinfo("showinfo", "Your group, " + groupname + ", has been created!")
+            self.addGroup(self.getGroupArray())
+            self.returnFromNewGroup()
+
+    def setGroupArray(self, groupname, m1, m2, m3, m4, m5):
+        self.groupArray = [groupname.get(), m1.get(), m2.get(),
+                      m3.get(), m4.get(), m5.get()]
+    
+    def getGroupArray(self):
+        return self.groupArray
 
     def createGroup(self):
         self.PostLoginFrame.place_forget()
@@ -145,41 +158,41 @@ class Application(Frame):
         self.CreateGroupFrame.configure(highlightbackground="#ffffff")
         self.CreateGroupFrame.configure(highlightcolor="black")
 
-        member1Label = Label(self.CreateGroupFrame, text='Group Owner: ', padx=5, pady=2)
+        member1Label = Label(self.CreateGroupFrame, text='Group Owner:', padx=5, pady=2)
         member1Label.grid(row=0, column=0)
         member1Entry = Entry(self.CreateGroupFrame)
         member1Entry.grid(row=0, column=1)
         member1Entry.insert(0, self.getUsername())
-        member2Label = Label(self.CreateGroupFrame, text='Member 2:       ', padx=5, pady=2)
+        member2Label = Label(self.CreateGroupFrame, text='Member 2:', padx=5, pady=2)
         member2Label.grid(row=1, column=0)
         member2Entry = Entry(self.CreateGroupFrame)
         member2Entry.grid(row=1, column=1)
-        member3Label = Label(self.CreateGroupFrame, text='Member 3:       ', padx=5, pady=2)
+        member3Label = Label(self.CreateGroupFrame, text='Member 3:', padx=5, pady=2)
         member3Label.grid(row=2, column=0)
         member3Entry = Entry(self.CreateGroupFrame)
         member3Entry.grid(row=2, column=1)
-        member4Label = Label(self.CreateGroupFrame, text='Member 4:       ', padx=5, pady=2)
+        member4Label = Label(self.CreateGroupFrame, text='Member 4:', padx=5, pady=2)
         member4Label.grid(row=3, column=0)
         member4Entry = Entry(self.CreateGroupFrame)
         member4Entry.grid(row=3, column=1)
-        member5Label = Label(self.CreateGroupFrame, text='Member 5:       ', padx=5, pady=2)
+        member5Label = Label(self.CreateGroupFrame, text='Member 5:', padx=5, pady=2)
         member5Label.grid(row=4, column=0)
         member5Entry = Entry(self.CreateGroupFrame)
         member5Entry.grid(row=4, column=1)
 
-        groupNameLabel = Label(self.CreateGroupFrame, text='Group Name:  ', padx=5, pady=10)
+        groupNameLabel = Label(self.CreateGroupFrame, text='Group Name:', padx=5, pady=10)
         groupNameLabel.grid(row=5, column=0)
         groupNameEntry = Entry(self.CreateGroupFrame)
         groupNameEntry.grid(row=5, column=1)
-        groupArray = [groupNameEntry.get(), member1Entry.get(), member2Entry.get(), 
-                      member3Entry.get(), member4Entry.get(), member5Entry.get()]
         self.submitNewGroupButton = Button(self.CreateGroupFrame,
                                            text="Submit",
-                                           command=lambda: self.checkGroupExists(groupNameEntry.get(), groupArray))
+                                           command=lambda: (self.setGroupArray(groupNameEntry, member1Entry, 
+                                                            member2Entry, member3Entry, member4Entry, member5Entry), 
+                                                            self.checkGroupExists(groupNameEntry.get(), self.getGroupArray())))
         self.submitNewGroupButton.place(relx=0.27, rely=0.7, relwidth=0.2, relheight=0.1)
         self.clearGroupEntriesButton = Button(self.CreateGroupFrame,
                                               text="Clear",
-                                              command=lambda: self.clearNewGroupEntries(member2Entry, 
+                                              command=lambda: self.clearNewGroupEntries(member2Entry,
                                               member3Entry, member4Entry, member5Entry, groupNameEntry))
         self.clearGroupEntriesButton.place(relx=0.52, rely=0.7, relwidth=0.2, relheight=0.1)
         self.returnFromNewGroupButton = Button(self.CreateGroupFrame,
@@ -227,8 +240,10 @@ class Application(Frame):
         self.viewGroupFrame.configure(background="#ffffff")
         self.viewGroupFrame.configure(highlightbackground="#ffffff")
         self.viewGroupFrame.configure(highlightcolor="black")
-        groupTopLabel = Label(self.viewGroupFrame, text='GROUP MEMBERS', padx=5, pady=2)
-        groupTopLabel.pack(side=TOP)
+        groupTopLabel = Label(self.viewGroupFrame, text=self.groupArray[0], padx=5, pady=2)
+        # groupTopLabel.place()
+        member1Label = Label(self.viewGroupFrame, text=self.groupArray[1], padx=5, pady=2)
+        member1Label.pack(side=TOP,ipadx=2, ipady=6)
 
     def returnFromNewGroup(self):
             self.CreateGroupFrame.place_forget()
@@ -243,7 +258,7 @@ class Application(Frame):
             self.ManageGroupFrame.place_forget()
             self.postLoginScreen()
 
-    # register_acc(self) Function: Takes (3) entries from user (username, 
+    # register_acc(self) Function: Takes (3) entries from user (username,
     # password, and password confirmation), ensures that the passwords
     # match, and submits the info to the backend so user can login.
     def register_acc(self):
@@ -272,10 +287,6 @@ class Application(Frame):
                                 command=lambda: (self.verifyPasswordRegister(user1, pass1, pass2)))
         self.submitreg.grid(row=3, column=2)
 
-    # def registerAccount(self):
-    #     username = self.register_acc.user1
-    #     password = self.register_acc.pass1
-    
     # verifyPasswordRegister(...) Function: Checks if passwords match.
     def verifyPasswordRegister(self, user1, pass1, pass2):
         if (pass1.get() == pass2.get()):
@@ -290,7 +301,7 @@ class Application(Frame):
             messagebox.showerror("showerror", "Passwords do not match. Try again.")
             pass1.delete(0, 'end')
             pass2.delete(0, 'end')
-    
+
     # createWidgets(self) Function: Creates the main widgets for the client.
     def createWidgets(self):
         self.MainMenuFrame = Frame(root, padx=5, pady=5)
